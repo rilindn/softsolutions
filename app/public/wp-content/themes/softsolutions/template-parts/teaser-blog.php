8455e043dadc914296eb8e91448e66131e6e8fff
florianalija <fa47799@ubt-uni.net>
2022-06-26 13:53:15 +0200

    blogs page added

--- app/public/wp-content/themes/softsolutions/template-parts/teaser-blog.php
+++ app/public/wp-content/themes/softsolutions/template-parts/teaser-blog.php
@@ -3,10 +3,34 @@
         <picture class="thumbnail">
             <?php the_post_thumbnail('medium') ?>
         </picture>
-        <div class="content">
+        <div class="blog-content">
             <?php the_title('<h1>', '</h1>'); ?>
             <div class="content-description">
                 <?php the_excerpt(); ?>
+            </div>
+            <hr class="divider" />
+            <div class="author">
+                <div class="avatar">
+                <!-- <?php 
+                    if(the_author()){
+                        $author = the_author();
+                        $author = ucfirst($author);
+                        $rest = substr($author, 0, 1);
+                        echo $rest;
+                    }
+                
+                ?> -->
+                </div>
+                <div class="author-date">
+                    <div class="author-section">
+
+                    <?php the_author();  ?>
+                    </div>
+                    <div class="date-section">
+
+                    <?php the_date(); ?>
+                    </div>
+                </div>
             </div>
         </div>
     </a>
