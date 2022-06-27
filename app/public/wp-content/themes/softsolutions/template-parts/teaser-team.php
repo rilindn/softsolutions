5d38d3071abd0ee66e426ebc8c33ef3990436f7b
rilindaxhaqkaj <rilinda@centralsoft.io>
2022-06-27 22:14:34 +0200

    feat: add team page

---
+++ app/public/wp-content/themes/softsolutions/template-parts/teaser-team.php
@@ -0,0 +1,12 @@
+<article class="team-post" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <header class="entry-header">
+            <picture class="thumbnail">
+                <?php the_post_thumbnail('medium') ?>
+            </picture>
+            <h1>
+                <?php 
+                    the_title()
+                ?>
+            </h1>
+        </header>
+</article>
