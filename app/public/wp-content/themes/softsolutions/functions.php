8c84ae881000443a2cb34c2369665185d592c139
rilind nuha <rn48707@ubt-uni.net>
2022-07-01 00:33:33 +0200

    feat: introduce get-in-touch widget

--- app/public/wp-content/themes/softsolutions/functions.php
+++ app/public/wp-content/themes/softsolutions/functions.php
@@ -4,6 +4,8 @@
 
     add_theme_support('post-thumbnails' );
 
+    add_theme_support('widgets');
+
     function wp_softsolutions_enque_scripts() {
         wp_enqueue_style( 'main-style', get_stylesheet_directory_uri() . './css/style.css' );
     }
@@ -25,4 +27,46 @@
         $res = json_decode($res, true);
         return $res;
     }
+
+    class wp_softsolutions_get_in_touch extends WP_Widget {
+ 
+        function __construct() {
+            parent::__construct(
+                'wp_softsolutions_get_in_touch', 
+
+                __('Get In Touch', 'softsolutions'), 
+
+                array( 'description' => __( 'Widget that provides user some instructions to contacting', 'softsolutions' ), )
+            );
+        }
+         
+        public function widget( $args, $instance ) {
+            echo $args['before_widget'];
+            
+            echo __( '
+                <div class="container">
+                    <div class="title-wrapper">
+                        <h1> We would love to hear your <span>ideas</span></h1>
+                    </div>
+                    <p class="sub">Whether you wish to launch a new app or website, integrate something new into your system or plan to increase capacity and efficiency with external support - we are excited to hear from you!</p>
+                    <div class="button-wrapper">
+                        <a href="/contact" class="button">Get in touch</a>
+                    </div>
+                </div>
+            ', 'softsolutions' );
+
+            echo $args['after_widget'];
+        }
+    } 
+    
+    function wp_softsolutions_init_widgets() {
+        register_widget( 'wp_softsolutions_get_in_touch' );
+        register_sidebar( [
+            'id' => 'footer',
+            'name' => esc_html__( 'Footer widget area', 'softsolutions' ),
+            'description' => esc_html__( 'Widget area made for footer', 'softsolutions' ),
+        ] );
+    }
+
+    add_action( 'widgets_init', 'wp_softsolutions_init_widgets' );
 ?>
